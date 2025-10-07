b0decdb1d12b5dcf7828d393fe5f02528d631671
vai101 <anand.vaibhav0@gmail.com>
2025-10-07 21:55:57 +0530

    error middleware added

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,19 @@
+// backend/middleware/errorMiddleware.js
+
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+    // Sometimes an error comes in with a 200 status code, so we'll default to 500
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
+};
+
+module.exports = { notFound, errorHandler };
